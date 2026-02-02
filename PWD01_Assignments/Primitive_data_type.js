dbbaada238365bca1e4699e9543036e2ff5a91b2
mohanrajtamilmaran <150466483+mohanrajtamilmaran@users.noreply.github.com>
2026-02-02 22:29:17 +0530

    Add files via upload
    week1_Assignment_Primitive_datatyes

---
+++ PWD01_Assignments/Primitive_data_type.js
@@ -0,0 +1,10 @@
+let userName = "Mohanraj"
+let organization = "Sukraa Software Solutions"
+let contactNumber = 8667662873
+let knowsAutomation = true
+let usesPlawright
+console.log("User Name: " + userName + " | Data Type: " + typeof(userName))
+console.log("Organization: " + organization + " | Data Type: " + typeof(organization))
+console.log("Contact Number: " + contactNumber + " | Data Type: " + typeof(contactNumber))
+console.log("Knows Automation: " + knowsAutomation + " | Data Type: " + typeof(knowsAutomation))
+console.log("Uses Playwright: " + usesPlawright + " | Data Type: " + typeof(usesPlawright))
